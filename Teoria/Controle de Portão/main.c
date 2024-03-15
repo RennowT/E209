0a06a69cef6decdb3189fe82132392003bd885ed
Matheus Renó Torres <124406908+RennowT@users.noreply.github.com>
2024-03-15 02:42:58 -0300

    Update main.c

--- Teoria/Controle de Portão/main.c
+++ Teoria/Controle de Portão/main.c
@@ -1,54 +1,65 @@
+#include <avr/io.h>
+#include <avr/delay.h>
+
 #define ABERTO 0
 #define ABRINDO 1
 #define FECHADO 2
 #define FECHANDO 4
 
+#define BT_FECHAR !(PIND & 0b00100000)
+#define BT_ABRIR !(PIND & 0b00010000)
+#define S_FECHADO !(PIND & 0b00001000)
+#define S_ABERTO !(PIND & 0b00000100)
+
 int main() {
-    unsigned char estadoPortao = FECHADO;
-    unsigned char MotorFechando, MotorAbrindo, SensorFechado, SensorAberto, ComandoFechar, ComandoAbrir;
 
-    while (1) {
-        switch (estadoPortao) {
-            case ABERTO:
-                MotorAbrindo = 0;
-                MotorFechando = 0;
-                if (ComandoFechar == 1) {
-                    estadoPortao = FECHANDO;
-                }
-                break;
+  DDRD |= 0b11000000;
+  DDRD &= ~(0b00111100);
 
-            case ABRINDO:
-                MotorAbrindo = 1;
-                MotorFechando = 0;
-                if (SensorAberto == 1) {
-                    estadoPortao = ABERTO;
-                } else if (ComandoFechar == 1) {
-                    estadoPortao = FECHANDO;
-                }
-                break;
+  PORTD |= 0b00111100;
 
-            case FECHADO:
-                MotorAbrindo = 0;
-                MotorFechando = 0;
-                if (ComandoAbrir == 1) {
-                    estadoPortao = ABRINDO;
-                }
-                break;
+  unsigned char estadoPortao = FECHADO;
 
-            case FECHANDO:
-                MotorAbrindo = 0;
-                MotorFechando = 1;
-                if (SensorFechado == 1) {
-                    estadoPortao = FECHADO;
-                } else if (ComandoAbrir == 1) {
-                    estadoPortao = ABRINDO;
-                }
-                break;
-            
-            default:
-                break;
+  while (1) {
+    switch (estadoPortao) {
+      case ABERTO:
+        PORTD &= ~(0b11000000);
+        if (BT_FECHAR) {
+            estadoPortao = FECHANDO;
         }
-    }
+        break;
+
+      case ABRINDO:
+        PORTD &= ~(0b10000000);
+        PORTD |= 0b01000000;
+        if (S_ABERTO) {
+            estadoPortao = ABERTO;
+        } else if (BT_FECHAR) {
+            estadoPortao = FECHANDO;
+        }
+        break;
+
+      case FECHADO:
+        PORTD &= ~(0b00000000);
+        if (BT_ABRIR) {
+            estadoPortao = ABRINDO;
+        }
+        break;
+
+      case FECHANDO:
+        PORTD &= ~(0b01000000);
+        PORTD |= 0b10000000;
+        if (S_FECHADO) {
+            estadoPortao = FECHADO;
+        } else if (BT_ABRIR) {
+            estadoPortao = ABRINDO;
+        }
+        break;
     
-    return 0;
+      default:
+        break;
+    }
+  }
+  
+  return 0;
 }
